9ed4215056eb467febad15474a9b46f0f30b3a42
asbeagle <asbeagle260@gmail.com>
2020-12-14 13:42:16 -0600

    tau and gamm cov & tau and alpha cov

---
+++ GEM_SIR_tau&gamma.cov.R
@@ -0,0 +1,139 @@
+## covariance in shedding/transmissabitly (tau) and gamma (sigmoidal)
+library(tidyverse)
+
+pick_individuals_multivariate <-function(N, traitmeans, traitsds, corr){
+  ## Generate Lognormal random variables with zeros. 
+  ## traitmeans - Vector of mean trait values
+  ## traitsds - Vector of trait standard deviations
+  ## R - Correlation matrix - should have 1s along the diagonal and off-diagonal elements reflecting correlations between different variables
+  
+  ## Number of traits
+  p <- length(traitmeans);
+  mu <- matrix(0,p,1);
+  sigma <- matrix(0,p,p);
+  for (i in 1:p) {
+    mu[i] = log(traitmeans[i]^2 / sqrt(traitsds[i]^2+traitmeans[i]^2))
+    sigma[i,i] = log(1 + traitsds[i]^2/traitmeans[i]^2);
+  }
+  
+  Cov <- sqrt(sigma)%*%corr%*%sqrt(sigma);
+  X <- matrix(rnorm(p*N,0,1), nrow = N, ncol = p);
+  for (i in 1:N){
+    X[i,] <- exp(mu + chol(Cov)%*%X[i,])
+  }      
+  return(X);
+}
+gillespie.SIR.cov_taugamma <- function(tmax, params, corr, x, seed=floor(runif(1,1,1e5))) {
+  set.seed(seed)
+  c = params["c"]
+  shed = params["shed"]
+  h = params["h"]
+  alpha = params["alpha"]
+  gamma = params["gamma"]
+  sd_s=params["sd_s"]
+  sd_g=params["sd_g"]
+  correlation=corr
+  b = params["b"]
+  bs = params["bs"]
+  d = params["d"]
+  
+  S=x["S"]
+  I=x["I"]
+  R=x["R"]
+  
+  ## draw the traits of our infected individuals
+  new_i <- pick_individuals_multivariate(I, traitmeans=c(shed,gamma), traitsds=c(sd_s,sd_g), corr=correlation) #infection
+  #print(new_i)
+  tau_i<-(new_i[,1]^2)/((h^2)+(new_i[,1]^2))
+  gamma_i <- new_i[,2]
+  
+  # start at time 0
+  t <- 0 
+  
+  results <- vector(mode='list', length=100000)
+  results[[1]] <- list(t, S, tau_i, gamma_i, R)
+  
+  ## row counter
+  i <- 2
+  
+  #start algorithm
+  while(t < tmax & length(tau_i) >0 ){ 
+    irate = c*tau_i*S
+    rrate = gamma_i*(length(tau_i))
+    brate <- (b - bs*(S+R+(length(tau_i)))) * (S+R+(length(tau_i)))
+    drateS <-S*(d)
+    drateI <-(d+alpha)*(length(tau_i))
+    drateR <- R*(d)
+    
+    rates<-c(irate,rrate,drateI,brate,drateS,drateR)
+    #print(rates)
+    ## what time does the event happen?
+    dt <- rexp(1, rate=sum(rates))
+    
+    ## update t 
+    t <- t + dt
+    
+    ## "wheel of fortune"
+    wheel <- cumsum(rates)/sum(rates)
+    
+    ## which event happens? Draw a random uniform to determine
+    rand <- runif(1)
+    
+    #(irate,drateI,rrate,brate,drateS,drateR)
+    
+    event <- 1 + sum(rand > wheel) 
+    if (event%in%1:length(tau_i)){### infection
+      S <- S-1
+      infection <- pick_individuals_multivariate(1,traitmeans=c(shed, gamma), traitsds=c(sd_s, sd_g), corr=correlation)
+      tau_i <- c(tau_i, (infection[,1]^2)/((h^2)+(infection[,1]^2))) # add to list of tau i
+      gamma_i <- c(gamma_i, infection[,2]) # add to list of gamma i
+    }
+    else if(event%in%((length(tau_i)+1):(length(tau_i)+length(gamma_i)))){ # recovery
+      R <- R+1
+      tau_i <- tau_i[-(event-length(gamma_i))]
+      gamma_i<- gamma_i[-(event-length(gamma_i))]
+    }
+    else if(event==(2*(length(tau_i)))+1){ # death of I
+      ind=sample(1:length(tau_i),1)
+      tau_i<-tau_i[-(ind)] 
+      gamma_i<-gamma_i[-(ind)]
+    } 
+    
+    else if (event==(2*(length(tau_i)))+2){ ### birth
+      S <- S+1
+    }
+    else if (event==(2*(length(tau_i)))+3) { ### death of S
+      S <- S-1
+    }
+    else    {   ### death of R
+      R <- R-1
+    }
+    
+    results[[i]] <- list(t, S, tau_i, gamma_i , R)
+    i <- i +1
+    
+  }  
+  
+  results <- results[1:(i-1)]
+  return(results)
+}
+
+x = c(S=70, I=10, R=0)
+tmax <- 150
+
+all.new.params2 = c(c=.2, shed=.2, sd_s=.01, sd_g=.01, h=.1, alpha=.01, gamma=.3, b=2.5, d=.4, bs=.01)
+corr <- matrix(c(1,0,0,1), nrow=2, byrow=T)
+
+
+new_out <- gillespie.SIR.cov_taugamma(tmax, all.new.params2, corr, x)
+
+#### output
+plot(unlist(lapply(new_out, function(y) y[[1]])), unlist(lapply(new_out, function(y) length(y[[3]]))), col="red",
+     type='l',lwd=1,xlab='Time', ylab="N", ylim=c(-5,200), xlim=c(0,150)) #main="beta=0.1, alpha=0.1, gamma=0.5,varB=1e-3, b=2.5, d=0.4")
+# plot number of susceptible
+lines(unlist(lapply(new_out, function(y) y[[1]])), unlist(lapply(new_out, function(y) y[[2]])),col="blue", 
+      type='l',lwd=1, xlab="Time", ylab="Number susceptible")
+# plot number of recovered
+lines(unlist(lapply(new_out, function(y) y[[1]])), unlist(lapply(new_out, function(y) y[[5]])), col="green",
+      type='l',lwd=1, xlab="Time", ylab="Number recovered")
+legend("topright",legend=c("S","I","R"),fill=c("blue","red","green"))
