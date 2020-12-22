a7fb62b0942b55a189c01cd5dded079b67d643cd
asbeagle <asbeagle260@gmail.com>
2020-12-22 13:27:08 -0600

    update determinisitic model and simulation plots

--- deterministic.SIR.R
+++ deterministic.SIR.R
@@ -6,6 +6,9 @@ deterministic.SIR=function(t,x,params){
   beta = params["beta"]
   alpha = params["alpha"]
   gamma = params["gamma"]
+  c = params["c"]
+  shed = params["shed"]
+  h = params["h"]
   b = params["b"]
   bs = params["bs"]
   d = params["d"]
@@ -14,8 +17,8 @@ deterministic.SIR=function(t,x,params){
   I=x[2]
   R=x[3]
   
-  dS=-beta*S*I + ((b-bs*(S+I+R))*(S+I+R)) - (d*(S))
-  dI= (beta*S*I)-(I*(alpha+gamma)) - (d*(I))
+  dS=-c*(shed/(h+shed))*S*I + ((b-bs*(S+I+R))*(S+I+R)) - (d*(S))
+  dI= c*(shed/(h+shed))*S*I-(I*(alpha+gamma)) - (d*(I))
   dR= gamma*I - (d*(R))
   
   list(c(dS,dI,dR))
@@ -27,13 +30,13 @@ param <-c(beta=.1, alpha=.01, gamma = .3, b=2, d=0.4, bs=0.01, ds=0.1)
 times = seq(0,100,by=1) # time steps to output 
 xstart = c(70,10,0) # beginning population size
 
-out=lsoda(xstart,times, deterministic.SIR, param)
+out=lsoda(xstart,times, deterministic.SIR, params)
 
 S=out[,2]
 I=out[,3]
 R=out[,4]
 
-plot.ts(S, col="red", ylim=c(0,130), ylab="N", xlab="Time")
-lines(I, col="blue")
+plot.ts(S, col="blue", ylim=c(0,200), ylab="N", xlab="Time")
+lines(I, col="red")
 lines(R, col="green")
-legend("topright",legend=c("S","I","R"),fill=c("red","blue","green"), cex=.7)
+legend("topright",legend=c("S","I","R"),fill=c("blue","red","green"), cex=.7)
